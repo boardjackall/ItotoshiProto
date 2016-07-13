c4a4177302f681579649a5fae0ffb9583684ee25
boardjackall <board.jackall@gmail.com>
2016-07-13 20:41:00 +0900

    変数を画面サイズから算出2

--- app/src/main/java/jp/gr/java_conf/jackall/itotoshi/itotoshi/DrawView.java
+++ app/src/main/java/jp/gr/java_conf/jackall/itotoshi/itotoshi/DrawView.java
@@ -66,7 +66,6 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback, Run
         y = new float[getWidth() / 2];
         for (i = 0; i < getWidth() / 2; i++) {
             y[i] = 0;
-
         }
     }
 
